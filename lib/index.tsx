1665a2b589afb6e32d4b357270d2773f61b55e18
JerichoDing <dingxihu@foxmail.com>
2019-09-15 01:14:52 +0800

    创建icon组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -9,11 +9,14 @@
  * @Author: Jericho Ding 
  * @Date: 2019-09-14 21:27:07 
  * @Last Modified by: Jericho Ding
- * @Last Modified time: 2019-09-14 22:56:12
+ * @Last Modified time: 2019-09-15 01:12:21
  */
 
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button';
-ReactDOM.render(<Button></Button>,document.getElementById("root"))
+import Icon from './icon';
+ReactDOM.render(
+  <div>
+    <Icon name="wechat" />
+  </div>,document.getElementById("root"))
