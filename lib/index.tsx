094610a0bc4c926d72b132e7820826448a3fd6e0
JerichoDing <dingxihu@foxmail.com>
2019-09-16 00:18:59 +0800

    eee

--- lib/index.tsx
+++ lib/index.tsx
@@ -12,19 +12,24 @@
  * @Last Modified time: 2019-09-15 10:44:51
  */
 
+// 测试代码
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Icon from './icon/icon';
+// const fn:React.MouseEventHandler = (e)=>{
+//   console.log(e);
+// }
+// ReactDOM.render(
+//   <div>
+//     <Icon name="wechat"
+//         onClick={fn}
+//         onMouseEnter={()=>{console.log('enter')}}
+//         onMouseLeave={()=>{console.log('leave')}}
+//     />
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-const fn:React.MouseEventHandler = (e)=>{
-  console.log(e);
-}
-ReactDOM.render(
-  <div>
-    <Icon name="wechat"
-        onClick={fn}
-        onMouseEnter={()=>{console.log('enter')}}
-        onMouseLeave={()=>{console.log('leave')}}
-    />
+//   </div>,document.getElementById("root"))
 
-  </div>,document.getElementById("root"))
+
+
+// 导出组件给其他人用
+export  {default as Icon } from './icon/icon';
